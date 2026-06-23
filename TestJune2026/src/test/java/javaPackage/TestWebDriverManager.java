56fe95aee78b36333c60dfc31e9c5716e2c5892e
admin <admin@DESKTOP-2JIFC99>
2026-06-23 13:43:32 +0530

    commented Webdriver lines

--- TestJune2026/src/test/java/javaPackage/TestWebDriverManager.java
+++ TestJune2026/src/test/java/javaPackage/TestWebDriverManager.java
@@ -15,11 +15,11 @@ public class TestWebDriverManager {
 	public void a()
 	{
 		System.out.println("First Git change");
-		WebDriverManager.chromedriver()	.setup();	
-		WebDriver driver= new ChromeDriver();
-		driver.manage().window().maximize();
+		//WebDriverManager.chromedriver()	.setup();	
+		//WebDriver driver= new ChromeDriver();
+		//driver.manage().window().maximize();
 		//driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);    //Webdrivv manager. dependency gone and upgraded vers. of Java
-        driver.get("https://paytm.com");
+        //driver.get("https://paytm.com");
 	    
 	}
 
